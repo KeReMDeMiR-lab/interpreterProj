4b23a697ba6a1abed0336d32354b6592623adea7
CyPlNkV <demirkerem1209@gmail.com>
2025-12-17 08:22:51 +0300

    added the main.go file

--- repl/repl.go
+++ repl/repl.go
@@ -13,7 +13,7 @@ const PROMPT = ">> "
 func Start(in io.Reader, out io.Writer){
 	scanner := bufio.NewScanner(in)
 	for {
-		fmt.Printf(PROMPT)
+		fmt.Println(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -22,7 +22,7 @@ func Start(in io.Reader, out io.Writer){
 		l := lexer.New(line)
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken(){
-			fmt.Println("%+v\n", tok)
+			fmt.Printf("%+v\n", tok)
 		}
 	}
 }
